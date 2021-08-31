ffeed47d4a192a7e521e2eb9612bc3758aa951a3
Dr. Moo <68446064+DrM00G@users.noreply.github.com>
2021-08-31 20:35:28 +0000

    board and cell classes functioning.

--- src/board.js
+++ src/board.js
@@ -1,21 +1,23 @@
 class Board {
   constructor(game,size=5){
-    var this.game = game;
-    var this.grid = {};
+    this.game = game;
+    this.grid = {};
     for (let i = 0; i < size; i++) {
       for (let j = 0; j < size; j++) {
-        this.grid(String((i,j)))=Cell((i,j))
+        this.grid[String([i,j])]= new Cell([i,j]);
+      }
     }
   }
 }
 
 class Cell {
   constructor(coord){
-    var coord = coord;
-    var units = [];
+    this.coord = coord;
+    this.units = [];
   }
 }
 
+module.exports = Board;
 
-var board=Board("Game")
-print(board.grid)
+// var board=Board("Game")
+// print(board.grid)
