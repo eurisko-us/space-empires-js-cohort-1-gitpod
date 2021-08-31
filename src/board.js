9233c6bd8dfeda08494f6d65bcfe0c385f5538fb
Riley R Paddock <67936293+RileyPaddock@users.noreply.github.com>
2021-08-31 20:57:00 +0000

    sortForCombat in Cell Class

--- src/board.js
+++ src/board.js
@@ -15,6 +15,26 @@ class Cell {
     this.coord = coord;
     this.units = [];
   }
+
+  sortForCombat(){
+    //Need to look at rules for exact combat ordering
+    function order(s1,s2){
+      let s1_grade = s1.technolgy['tactics'] + s1.attack_class;
+      let s2_grade = s2.technolgy['tactics'] + s2.attack_class;
+      if (s1_grade > s2_grade){
+        return 1;
+      }else if(s1_grade < s2_grade){
+        return -1;
+      }else{
+        if (s1.playerIndex < s2.playerIndex){
+          return 1;
+        }else{
+          return -1;
+        }
+      }
+    }
+    return this.units.sort(order(s1,s2));
+  }
 }
 
 module.exports = Board;
