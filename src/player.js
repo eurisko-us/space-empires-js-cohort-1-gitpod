761ef8bf87a59129111deeea314a3ad7b29d051a
geomeza <67932202+geomeza@users.noreply.github.com>
2021-10-05 20:03:47 +0000

    reworked player build function

--- src/player.js
+++ src/player.js
@@ -6,35 +6,37 @@ const ColonyShip = require("../src/units/colony-ship")
 const DefaultStrategy = require("../src/strategies/default-strategy")
 
 class Player {
-  constructor(strategy, position, boardSize, playerIndex, playerColor) {
+  constructor(strategy, coord, boardSize, playerIndex, playerColor) {
     this.strategy = new strategy(playerIndex);
     this.creds = 27;
     this.boardSize = boardSize;
     this.technology = { "attack": 0, "defense": 0, "movement": 1, "shipsize": 1, "shipyard": 1, "terraform": 0, "tactics": 0, "exploration": 0 };
-    this.homeBase = new Colony(playerIndex, position, 0, this.technology, 0, true);
+    this.homeBase = new Colony(playerIndex, coord, 0, this.technology, 0, true);
     this.playerIndex = playerIndex;
     this.playerColor = playerColor;
-    this.units = []
+    this.units = [];
+    this.id_number = 0;
   }
 
-  build(game, unit) { // Unit is formatted as ["string of ship type", (tuple of position)]
+  build(game, unit) { // Unit is formatted as ["string of ship type", (tuple of coord)]
     let unitTypes = {"Scout": Scout, "Destroyer": Destroyer,/*, more fighting ships later */ "Colony Ship": ColonyShip}
     // console.log(unit)
     // console.log(unit[1]);
-    // console.log(this.getPossibleBuildPositions())
+    // console.log(this.getPossibleBuildCoords())
+    let possibleBuildPositions = this.getPossibleBuildCoords()
     let unitBuildCoords = JSON.stringify(unit[1])
-    let possibleBuildPositions = this.getPossibleBuildPositions()
     if (!possibleBuildPositions.includes(unitBuildCoords))
       throw `Player ${this.playerIndex} tried to cheat by 
             building a ${unit[0]} in an invalid hex at ${unit[1]}`; 
-    let newShip = new unitTypes[unit[0]](/* stuff */);
+    let newShip = new unitTypes[unit[0]](this.playerIndex, unit[1], this.id_number, this.technology, game.turn);
+    this.id_number += 1;
     this.units.push(newShip);
+    game.board.grid[String(unit[1])].appendUnitReference(newShip.generateState(true, false));
   }
 
-  getPossibleBuildPositions() {
+  getPossibleBuildCoords() {
     return [JSON.stringify(this.homeBase.coords)];
   }
-
 
   upgrade(game, tech) { // Tech is formmated as a string
     // Upgrade Attack Technology
